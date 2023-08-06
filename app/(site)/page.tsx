daf1092ccfaad6139a43a212fad8198269ded9f5
Alvaro Peña <alvpe26@gmail.com>
2023-08-06 16:37:54 -0500

    Basic set up

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -7,9 +7,7 @@ export default async function Home() {
 
   return (
     <div>
-      <h1 className="text-7xl font-extrabold">Hello I&apos;m
-        <span className="bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 bg-clip-text text-transparent"> Kapehe!</span>
-      </h1>
+
       <p className="mt-3 text-xl text-gray-600">Aloha everyone! Check out my projects!</p>
       <h2 className="mt-24 font-bold text-gray-700 text-3xl">My Projects</h2>
 
--- sanity/config/client-config.ts
+++ sanity/config/client-config.ts
@@ -1,5 +1,5 @@
 const config = {
-  projectId: "txd29ab2",
+  projectId: "eponkvgi",
   dataset: "production",
   apiVersion: "2023-03-09",
 }
