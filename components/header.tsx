a97eb7f87c799a22620b73a4c0b22c10747eeefa
unknown <alvpe26@gmail.com>
2023-08-27 18:07:43 -0500

    Fixing the header dropdown menu

--- components/header.tsx
+++ components/header.tsx
@@ -1,18 +1,33 @@
 "use client";
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import Link from "next/link";
 import { ModeToggle } from "./mode-toggle";
 import { Menu } from "lucide-react";
 
 const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const dropdownRef = useRef(null);
+
+  useEffect(() => {
+    const handleDocumentClick = (event) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        setIsMenuOpen(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleDocumentClick);
+
+    return () => {
+      document.removeEventListener("mousedown", handleDocumentClick);
+    };
+  }, []);
 
   return (
     <header className="flex items-center justify-between m-5">
       <Link href="/" className="font-semibold text-2xl">
         Alvaro Peña
       </Link>
-      <div className="flex flex-row gap-3 items-center">
+      <div className="relative flex flex-row gap-3 items-center">
         <Link href="/projects" className="hidden md:block">
           Projects
         </Link>
@@ -27,17 +42,21 @@ const Header = () => {
           <Menu />
         </button>
         {isMenuOpen && (
-          <div className="absolute top-12 right-5 w-48 bg-white shadow-md rounded-md p-6">
-            <Link href="/" className="block mb-2 hover:text-blue-500">
-              Home
-            </Link>
-            <Link href="/projects" className="block mb-2 hover:text-blue-500">
-              Projects
-            </Link>
-            <Link href="/essays" className="block hover:text-blue-500">
-              Essays
-            </Link>
-          </div>
+          <div
+          ref={dropdownRef}
+          className="absolute top-full right-0 w-48 bg-white dark:bg-secondary shadow-md rounded-md p-6 mt-2"
+        >
+          <Link href="/" className="block mb-2 hover:text-blue-500">
+            Home
+          </Link>
+          <Link href="/projects" className="block mb-2 hover:text-blue-500">
+            Projects
+          </Link>
+          <Link href="/essays" className="block hover:text-blue-500">
+            Essays
+          </Link>
+        </div>
+        
         )}
       </div>
     </header>
