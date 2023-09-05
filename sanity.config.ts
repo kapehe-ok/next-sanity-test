e375d4618fe8013d5174ef12c743dded32707158
anhuet <volocanh102@gmail.com>
2023-09-05 12:49:21 +0700

    add deploy tool

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,14 +1,14 @@
 import { defineConfig } from "sanity";
 import { deskTool } from "sanity/desk";
 import schemas from "./sanity/schemas";
-
+import { vercelDeployTool } from 'sanity-plugin-vercel-deploy'
 const config = defineConfig({
   projectId: "eponkvgi",
   dataset: "production",
   title: "My Web",
   apiVersion: "2023-06-08",
   basePath: "/admin",
-  plugins: [deskTool()],
+  plugins: [deskTool(), vercelDeployTool()],
   schema: { types: schemas },
 });
 
