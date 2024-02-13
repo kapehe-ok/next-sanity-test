4fb4ff91854d5ae0612731a58b8a952387ea8083
alvropena <me@alvropena.com>
2024-02-12 23:11:06 -0800

    Removed dropdown menu from header

--- components/header.tsx
+++ components/header.tsx
@@ -1,16 +1,6 @@
-import React, { useState, useEffect, useRef } from "react";
+import React from "react";
 import Link from "next/link";
 import { ModeToggle } from "./mode-toggle";
-import { Menu } from "lucide-react";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
-import { Button } from "./ui/button";
 
 
 const Header = () => {
@@ -20,36 +10,7 @@ const Header = () => {
         Alvaro Peña
       </Link>
       <div className="relative flex flex-row gap-3 items-center">
-
-
         <ModeToggle />
-
-        <div className="md:hidden">
-          <DropdownMenu>
-            <DropdownMenuTrigger>
-              <Button variant={"outline"}>
-                <Menu />
-              </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent align="end">
-              <Link href={"/"}>
-                <DropdownMenuItem>
-                  Home
-                </DropdownMenuItem>
-              </Link>
-              <Link href={"/projects"}>
-                <DropdownMenuItem>
-                  Projects
-                </DropdownMenuItem>
-              </Link>
-              <Link href={"/essays"}>
-                <DropdownMenuItem>
-                  Essays
-                </DropdownMenuItem>
-              </Link>
-            </DropdownMenuContent>
-          </DropdownMenu>
-        </div>
       </div>
     </header>
   );
