c2710de52f51cc12efa97df8d04599f535b00580
alvropena <me@alvropena.com>
2024-02-13 00:03:07 -0800

    Fixing bugs

--- next.config.js
+++ next.config.js
@@ -3,15 +3,6 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'cdn.sanity.io',
-        port: '',
-      },
-    ],
-  },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
