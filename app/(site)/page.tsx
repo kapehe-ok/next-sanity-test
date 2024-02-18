793655dd473dd9aa34aaf5eca3d373d235716949
alvropena <me@alvropena.com>
2024-02-18 12:03:08 -0800

    Fixing

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -4,9 +4,8 @@ import Link from "next/link"; // Import the Link component
 export default function Home() {
   return (
     <div className="flex flex-col h-screen gap-3 m-5 text-md">
-      <p>Hello, world.</p>
       {/* Use the Link component for client-side navigation */}
-      <p>Try Education <Link href="/education" className="text-blue-500 underline">here</Link>.</p>
+      <p>Master any topic playing <Link href="/education" className="text-blue-500 underline">here</Link>.</p>
     </div>
   );
 }
