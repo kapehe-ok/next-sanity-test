9a03c7049c4853fe65b18d487cbbfc216888e88a
alvropena <me@alvropena.com>
2024-02-18 12:07:08 -0800

    Updating footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,4 +1,4 @@
-import { Github, Linkedin, Twitter, Youtube } from "lucide-react";
+"use client";
 import React from "react";
 import { GitHubLogoIcon, LinkedInLogoIcon, TwitterLogoIcon } from "@radix-ui/react-icons";
 
@@ -6,17 +6,19 @@ const Footer = () => {
   return (
     <footer className="flex flex-row items-center justify-between m-5 pb-5">
       <div className="flex flex-row gap-3">
-        <a href="https://www.linkedin.com/in/alvropena/">
+        <a href="https://www.linkedin.com/in/alvropena/" target="_blank" rel="noopener noreferrer">
           <LinkedInLogoIcon />
         </a>
-        <a href="https://github.com/alvropena">
+        <a href="https://github.com/alvropena" target="_blank" rel="noopener noreferrer">
           <GitHubLogoIcon />
         </a>
-        <a href="https://www.x.com/alvropenaa">
+        <a href="https://www.x.com/alvropenaa" target="_blank" rel="noopener noreferrer">
           <TwitterLogoIcon />
         </a>
       </div>
-      <p className="text-blue-500 text-sm">me@alvropena.com</p>
+      <a href="mailto:me@alvropena.com" className="text-blue-500 text-sm">
+        me@alvropena.com
+      </a>
     </footer>
   );
 };
