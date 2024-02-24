5b778d7ad8adc8bfcf0868943ef60574047e32ca
alvropena <me@alvropena.com>
2024-02-23 23:31:18 -0800

    Updating projects website

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -8,15 +8,13 @@ export default function Home() {
       <p>Master any topic playing <Link href="/education" className="text-blue-500 underline">here</Link>.</p>
       <p className="text-lg">About Me</p>
       <p>
-        I was born and raised in Lima, Peru. I studied Economics & Finance for four years at Universidad del Pacífico,
-        but dropped out at the beginning of 2023 - on my last year. After gaining traction with my first startup idea,
-        I arrived to the conclusion that I wanted to focus on education as a problem and I didn&apos;t felt that
-        college was the route for me. Eventually, the startup failed, but I managed to convience my parents to transfer
-        to Cal State, East Bay so I could be as close as possible to SF - the biggest tech hub in the world. In 2024, on the 9th
-        of January, I arrived to the States, transferred as an upper division student and worked for a year remotely with software
-        projects. Below, you can see them organized by dates.
+
       </p>
       <p className="text-lg">Projects</p>
+      <p>Education</p>
+      <p>Managent</p>
+      <p>Bhuma AI</p>
+      <p>Dudda</p>
     </div>
   );
 }
