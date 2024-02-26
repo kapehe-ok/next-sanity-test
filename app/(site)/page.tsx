082c9c783b08681828562a6808cc61ce88a7be9d
alvropena <me@alvropena.com>
2024-02-26 02:39:37 -0800

    Cleaning up the index page

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -6,15 +6,6 @@ export default function Home() {
     <div className="flex flex-col h-screen gap-3 m-5 text-md">
       {/* Use the Link component for client-side navigation */}
       <p>Master any topic playing <Link href="/education" className="text-blue-500 underline">here</Link>.</p>
-      <p className="text-lg">About Me</p>
-      <p>
-
-      </p>
-      <p className="text-lg">Projects</p>
-      <p>Education</p>
-      <p>Managent</p>
-      <p>Bhuma AI</p>
-      <p>Dudda</p>
     </div>
   );
 }
